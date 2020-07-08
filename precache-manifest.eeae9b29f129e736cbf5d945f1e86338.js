d70a0795356fd81a1777b1fb1099d67eb46c9e48
unknown <dschleicherwbs@gmail.com>
2020-07-08 21:42:22 +0200

    first commit

---
+++ precache-manifest.eeae9b29f129e736cbf5d945f1e86338.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bf025b4aefd92b73b5",
+    "url": "/css/about.00535270.css"
+  },
+  {
+    "revision": "44b8e37bf3140f5c4289",
+    "url": "/css/app.42dac473.css"
+  },
+  {
+    "revision": "4915146311da4991109cb8e038526124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51bf025b4aefd92b73b5",
+    "url": "/js/about.fa3bbee7.js"
+  },
+  {
+    "revision": "44b8e37bf3140f5c4289",
+    "url": "/js/app.7437b661.js"
+  },
+  {
+    "revision": "e7ecbc403ce485ea137c",
+    "url": "/js/chunk-vendors.a4b14e59.js"
+  },
+  {
+    "revision": "002ae7f90fc33282e627e95c01b9c9a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
